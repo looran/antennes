841d962ebe21ee69292cf90d4dc3c9f27daf7e27
Laurent Ghigonis <ooookiwi@gmail.com>
2024-03-21 09:29:51 +0100

    remove useless old comment

--- antennes.c
+++ antennes.c
@@ -123,8 +123,6 @@ struct exploitant {
  * | 0 1 2   3 4 5   6 7 8 9 |
  * |-------------------------|
  * |  dept |  zone |   id    |
- * |-------------------------|
- * |    area       |   id    |
  * ---------------------------
  * STA_NM_ANFR is mapped to sta_nm structure
  * storage is in f_station:
