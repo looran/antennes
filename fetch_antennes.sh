fa6fd29e84a71cfcec00670788bedcb91097f0b0
Laurent Ghigonis <ooookiwi@gmail.com>
2024-10-08 00:04:47 +0200

    fetch: better fixes for incorrect datasets since 2015

--- fetch_antennes.sh
+++ fetch_antennes.sh
@@ -25,14 +25,18 @@ cat $DL_DIR/urls.txt |egrep "\.zip;" |while IFS=";" read published title url sum
     period="$(echo $published |cut -d'-' -f1-2)"
     if [ -n "$lastperiod" -a "$lastperiod" != "$period" ]; then
         sets_done=$(($sets_done+1))
+	# fix 2016-08: missing files, use from previous month
         [ $lastperiod = "2016-08" ] \
             && trace cp $EXTRACT_DIR/2016-08/20160730_DATA/* $EXTRACT_DIR/2016-08/
+	# fix 2017-01: missing files, use from previous month
         [ $lastperiod = "2017-01" ] \
             && trace cp $EXTRACT_DIR/2017-02/{SUP_EXPLOITANT.txt,SUP_NATURE.txt,SUP_PROPRIETAIRE.txt,SUP_TYPE_ANTENNE.txt} $EXTRACT_DIR/2017-01
+	# fix 2018-03: file name is inconsistant
         [ $lastperiod = "2018-03" ] \
             && (cd $EXTRACT_DIR/2018-03 && 7z -y x 20180228_Export_Etalab_Data.zip >/dev/null)
         [ $sets_done -eq $sets_count ] && break
     fi
+    # fetch
     filename=$(basename $url)
     echo "[set $(($sets_done+1))/$sets_count] '$title' for period '$period' filename '$filename'"
     [ -e $DL_DIR/$filename -o $offline -eq 1 ] \
@@ -41,6 +45,20 @@ cat $DL_DIR/urls.txt |egrep "\.zip;" |while IFS=";" read published title url sum
     [ "$sum" != "$(trace sha1sum $DL_DIR/$filename |cut -d' ' -f1)" ] \
         && echo "error: checksum verification failed for $DL_DIR/$filename" \
         && exit 1
+    # fix 2015-04/05: file names are same between april and may
+    [ $period = "2015-04" -a $filename = "Tables_de_Reference.zip" ] \
+        && trace mv $DL_DIR/Tables_de_Reference.zip $DL_DIR/Tables_de_Reference_2015-04.zip \
+	&& filename=Tables_de_Reference_2015-04.zip
+    [ $period = "2015-04" -a $filename = "Tables_supports_antennes_emetteurs_bandes.zip" ] \
+        && trace mv $DL_DIR/Tables_supports_antennes_emetteurs_bandes.zip $DL_DIR/Tables_supports_antennes_emetteurs_bandes_2015-04.zip \
+	&& filename=Tables_supports_antennes_emetteurs_bandes_2015-04.zip
+    [ $period = "2015-05" -a $filename = "Tables_de_Reference.zip" ] \
+        && trace mv $DL_DIR/Tables_de_Reference.zip $DL_DIR/Tables_de_Reference_2015-05.zip \
+	&& filename=Tables_de_Reference_2015-05.zip
+    [ $period = "2015-05" -a $filename = "Tables_supports_antennes_emetteurs_bandes.zip" ] \
+        && trace mv $DL_DIR/Tables_supports_antennes_emetteurs_bandes.zip $DL_DIR/Tables_supports_antennes_emetteurs_bandes_2015-05.zip \
+	&& filename=Tables_supports_antennes_emetteurs_bandes_2015-05.zip
+    # extract
     mkdir -p $EXTRACT_DIR/$period
     trace 7z -aos -y x -o$EXTRACT_DIR/$period $DL_DIR/$filename >/dev/null
     lastperiod=$period
